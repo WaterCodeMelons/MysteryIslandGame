fcbb889b6aed90f19d4d700038f8e1c0ab00ca03
Aleksander Ujek <aleksander.ujek@gmail.com>
2017-11-10 21:51:44 +0100

    Menu Control Basics - movement with arrows - moving between panels - simple scroll view option

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,30 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
-public class MenuController : MonoBehaviour {
+public class MenuController : MonoBehaviour
+{
 
-	public Transform menu; 
-	public Transform player;
-	
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Escape)) { // Jeżeli podczas gry klikniemy Escape to przechodzi do metody Pause()
-			Pause();
-		}
-	}
+    public Transform menu;
+    public Transform player;
 
-	public void Pause () {
-		if (menu.gameObject.activeInHierarchy == false) { // Jeżeli menu nie jest aktywne w hierarchii
-			menu.gameObject.SetActive(true); // Ustaw menu jako aktywny element
-			Time.timeScale = 0; // Zatrzymaj czas
-			player.GetComponent<FirstPersonController>().enabled = false; // Wyłącz działanie FPC
-		} else {
-			menu.gameObject.SetActive(false); // Ustaw menu jako nieaktywny element
-			Time.timeScale = 1; // Włącz czas
-			player.GetComponent<FirstPersonController>().enabled = true; // Pozwól na działanie FPC
-		}
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        { // Jeżeli podczas gry klikniemy Escape to przechodzi do metody Pause()
+            Pause();
+        }
+    }
 
-	public void Quit () {
+    public void Pause()
+    {
+        if (menu.gameObject.activeInHierarchy == false)
+        { // Jeżeli menu nie jest aktywne w hierarchii
+            menu.gameObject.SetActive(true); // Ustaw menu jako aktywny element
+            Time.timeScale = 0; // Zatrzymaj czas
+            Cursor.visible = true;
+            player.GetComponent<FirstPersonController>().enabled = false; // Wyłącz działanie FPC
+        }
+        else
+        {
+            menu.gameObject.SetActive(false); // Ustaw menu jako nieaktywny element
+            Time.timeScale = 1; // Włącz czas
+            Cursor.visible = false;
+            player.GetComponent<FirstPersonController>().enabled = true; // Pozwól na działanie FPC
+        }
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
 		Application.Quit(); // Wyjdź z gry
+#endif
+
 	}
 }
