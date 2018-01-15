58c150f55ab683f02766fa0e78b404dcc6135325
WlodarczykKamil <kam9611@gmail.com>
2018-01-15 23:27:24 +0100

    Map working in real-time. Press 'M' key to open or close MapMenu.

---
+++ Assets/Scripts/MapMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMenuController : MonoBehaviour {
+
+    public Transform MapMenu;
+    public Transform Player;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (MapMenu.gameObject.activeInHierarchy == false)
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                MapMenu.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.M))
+            {
+                MapMenu.gameObject.SetActive(false);
+            }
+        }
+
+        
+		
+	}
+}
